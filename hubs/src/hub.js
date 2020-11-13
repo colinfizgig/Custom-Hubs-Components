c65625626870249dcec3f94ee347ae61701b177d
William Freeman <colinfizgig@gmail.com>
2020-11-13 13:58:36 -0500

    Update hub.js
    fixed the execution order for the script injection so that objects network correctly

--- hubs/src/hub.js
+++ hubs/src/hub.js
@@ -659,96 +659,18 @@ function handleHubChannelJoined(entryManager, hubChannel, messageDispatch, data)
 
           return;
         });
-    };
+		
+		// >>>>> Once the loadEnvironmentAndConnect() function runs we can inject the custom client stuff
+		finishInjection(); // script injection function added below
 
-	////////////////////////////////////////////////////////////////////////////////////////
-	// injected stuff here
-	////////////////////////////////////////////////////////////////////////////////////////
+    }
+
+	window.APP.hub = hub;
+	updateUIForHub(hub, hubChannel);
+	scene.emit("hub_updated", { hub });
 	
-	console.log("checking access");
-	console.log(hub.hub_id);
-		//check to see if the scene exists here
-		//let scene = document.querySelector("a-scene");
-		
-		// set the version of the components from github commit hash
-		//let version = "43b68247dfb47c581d307f413a055a7fd4d9ba31";
-		//local host check to our Node server.  Should be to node server at some point.
-		//const myScene = "Y8SYx7W";
-		//https://localhost:8080/hub.html?hub_id=2MCVA2f is a hub that contains a good scene
-		//for testing purposes.
-		const myHub = hub.hub_id;
-
-		const url = "http://localhost:3000/injectScripts?hubid="+ myHub;
-		
-		function makeRequest (method, url) {
-			
-			  return new Promise(function (resolve, reject) {
-				var xhr = new XMLHttpRequest();
-				xhr.open(method, url);
-				xhr.onload = function () {
-				  if (this.status >= 200 && this.status < 300) {
-					resolve(xhr.response);
-				  } else {
-					reject({
-						status: this.status,
-						statusText: xhr.statusText
-					});
-				  }
-				};
-				xhr.onerror = function () {
-					reject({
-						status: this.status,
-						statusText: xhr.statusText
-					});
-				};
-				xhr.send();
-			  });
-			}
-
-			makeRequest('GET', url)
-			//fetch(url,{method:'GET', headers: {
-			//'Content-type': 'text/html; charset=UTF-8'}})
-				.then(function (datums) {
-					console.log("got server hub response");
-					console.log(datums);
-					
-					var myUrls = datums.split(",");
-					var myBody = document.querySelector("body");
-					for(var items of myUrls) {
-						if(items == "noUrls"){
-							break;
-						}
-						console.log("iterating");
-						//inject some scripts based on the returned urls (we may need to split a list or something.
-						var newScript = document.createElement("script");
-						newScript.type = 'text/javascript';
-
-						var srcAt = document.createAttribute('src');
-						srcAt.value = items;
-						newScript.setAttributeNode(srcAt);
-
-						myBody.appendChild(newScript);
-					}
-					
-					finishInjection();
-					
-					resolve();
-				})
-				.catch(function (err) {
-					finishInjection();
-					console.error('Augh, there was an error!', err.statusText);
-				});
-
-	///////////////////////////////////////////////////////////////////////////////////////
-
-	
-	function finishInjection(){
-		
-		window.APP.hub = hub;
-		updateUIForHub(hub, hubChannel);
-		scene.emit("hub_updated", { hub });
-
-		if (!isEmbed) {
+	// this needs to occur before script injection or the components will not network
+	if (!isEmbed) {
 		  loadEnvironmentAndConnect();
 		} else {
 		  remountUI({
@@ -759,9 +681,79 @@ function handleHubChannelJoined(entryManager, hubChannel, messageDispatch, data)
 			}
 		  });
 		}
+		
+	////////////////////////////////////////////////////////////////////////////////////////
+	// script injection function which was called above
+	////////////////////////////////////////////////////////////////////////////////////////
+	function finishInjection(){
+
+		console.log("checking access");
+		console.log(hub.hub_id);
+
+		//local host check to our Node server.  Should be to node server at some point.
+		//https://localhost:8080/hub.html?hub_id=2MCVA2f is a hub that contains a good scene
+		//for testing purposes.
+		const myHub = hub.hub_id;
+
+		const url = "http://localhost:3000/injectScripts?hubid="+ myHub;
+			
+		function makeRequest (method, url) {
+				
+			return new Promise(function (resolve, reject) {
+				var xhr = new XMLHttpRequest();
+				xhr.open(method, url);
+				xhr.onload = function () {
+					if (this.status >= 200 && this.status < 300) {
+						resolve(xhr.response);
+					} else {
+						reject({
+							status: this.status,
+							statusText: xhr.statusText
+						});
+					}
+				};
+				xhr.onerror = function () {
+					reject({
+						status: this.status,
+						statusText: xhr.statusText
+					});
+				};
+				xhr.send();
+			});
+		}
+
+		makeRequest('GET', url)
+		.then(function (datums) {
+			console.log("got server hub response");
+			console.log(datums);
+						
+			var myUrls = datums.split(",");
+			var myBody = document.querySelector("body");
+			for(var items of myUrls) {
+				if(items == "noUrls"){
+					break;
+				}
+				console.log("iterating");
+				//inject some scripts based on the returned urls (we may need to split a list or something.
+				var newScript = document.createElement("script");
+				newScript.type = 'text/javascript';
+
+				var srcAt = document.createAttribute('src');
+				srcAt.value = items;
+				newScript.setAttributeNode(srcAt);
+
+				myBody.appendChild(newScript);
+			}
+			resolve();
+		})
+		.catch(function (err) {
+			console.error('Augh, there was an error!', err.statusText);
+		});	
 	}
 	
-	
+	///////////////////////////////////////////////////////////////////////////////////////	
+	// 	finish injected scripts stuff
+	///////////////////////////////////////////////////////////////////////////////////////
   };
   connectToScene();
 }

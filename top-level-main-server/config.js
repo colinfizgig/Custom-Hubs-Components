8c4770d35c6cc8afc9a2c6ed07698fe7aba18987
William Freeman <colinfizgig@gmail.com>
2020-11-15 15:14:37 -0500

    moving client load to server created script

--- top-level-main-server/config.js
+++ top-level-main-server/config.js
@@ -9,7 +9,7 @@ var config = {};
 
 config.hubsarray = [
 	{hub_id: '2MCVA2f', urls:'https://colinfizgig.github.io/Custom-Hubs-Components/components/camera-cube-env.js,https://colinfizgig.github.io/Custom-Hubs-Components/components/slideconfig.js,https://colinfizgig.github.io/Custom-Hubs-Components/components/hubs-slide-show.js,https://colinfizgig.github.io/Custom-Hubs-Components/components/interactable-ball.js'},
-	{hub_id: '8T88rLN', urls:'https://colinfizgig.github.io/Custom-Hubs-Components/components/hubs-google-slideshow.js'},
+	{hub_id: '8T88rLN', urls:'https://colinfizgig.github.io/Custom-Hubs-Components/components/hubs-google-slideshow.js,https://colinfizgig.github.io/Custom-Hubs-Components/components/gslides-kicker.js'},
 	{hub_id: '5HsuTZ2', urls:'https://colinfizgig.github.io/Custom-Hubs-Components/components/slideconfig.js,https://colinfizgig.github.io/Custom-Hubs-Components/components/hubs-slide-show.js'},
 	{hub_id: 'kBquF8A', urls:'https://colinfizgig.github.io/Custom-Hubs-Components/components/camera-cube-env.js,https://colinfizgig.github.io/Custom-Hubs-Components/components/interactable-ball.js'},
 	{hub_id: 'DVeQzPW', urls:'https://colinfizgig.github.io/Custom-Hubs-Components/components/camera-cube-env.js,https://colinfizgig.github.io/Custom-Hubs-Components/components/interactable-ball.js'}

5350175e0c055c9cfcbcc642f1f60466a9f7df1b
William Freeman <colinfizgig@gmail.com>
2020-11-17 15:33:53 -0500

    Update hubs-pdf-show.js

--- components/hubs-pdf-show.js
+++ components/hubs-pdf-show.js
@@ -1,7 +1,7 @@
 const pdfURL = "https://hubs-proxy.com/https://colinfizgig.github.io/Custom-Hubs-Components/components/Binder1.pdf";
 
 function addPdf(){
-	var el = document.createElement("a-entity")'
+	var el = document.createElement("a-entity");
 	el.setAttribute("networked", { template: "#pdfshow-media" } );
 	el.setAttribute("media-loader", {src:pdfURL, animate: false, fileIsOwned: true});
 	el.object3D.position.y = 2;

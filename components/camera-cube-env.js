7fada20c7ac8a0cf23c4233dfb2184c714d0e758
William Freeman <colinfizgig@gmail.com>
2020-11-28 14:59:01 -0500

    Update camera-cube-env.js

--- components/camera-cube-env.js
+++ components/camera-cube-env.js
@@ -36,7 +36,7 @@ AFRAME.registerComponent('camera-cube-env', {
 		this.myCamEntity = document.createElement('a-entity');
 		this.myCamEntity.class = "cubeCam";
 	    this.myCamEntity.add( this.cam );
-		myScene.add(this.myCamEntity);
+		myScene.appendChild(this.myCamEntity);
 
 	    this.done = false;
 		
@@ -55,7 +55,7 @@ AFRAME.registerComponent('camera-cube-env', {
 				AFRAME.scenes[0].renderer.autoClear = true;
 				var camVector = new THREE.Vector3();
 				myEl.object3D.getWorldPosition(camVector);
-				this.myCamEntity.el.object3D.position.copy(myEl.object3D.worldToLocal(camVector));
+				this.myCamEntity.position.copy(myEl.object3D.worldToLocal(camVector));
 				myCam.update( AFRAME.scenes[0].renderer, myEl.sceneEl.object3D );
 
 				if (node.type.indexOf('Mesh') !== -1) {
@@ -93,7 +93,7 @@ AFRAME.registerComponent('camera-cube-env', {
 	        AFRAME.scenes[0].renderer.autoClear = true;
 			var camVector = new THREE.Vector3();
 			myEl.object3D.getWorldPosition(camVector);
-	        this.myCamEntity.el.object3D.position.copy(myEl.object3D.worldToLocal(camVector));
+	        this.myCamEntity.position.copy(myEl.object3D.worldToLocal(camVector));
 	        myCam.update( AFRAME.scenes[0].renderer, myEl.sceneEl.object3D );
 			if(myMesh){
 				myMesh.traverse( function( child ) { 

868aa631ea929f774284ade0ba4b5caf80c2b472
William Freeman <colinfizgig@gmail.com>
2020-12-10 10:43:23 -0500

    Update camera-cube-env.js

--- components/camera-cube-env.js
+++ components/camera-cube-env.js
@@ -51,6 +51,7 @@ AFRAME.registerComponent('camera-cube-env', {
 
 				if (node.type.indexOf('Mesh') !== -1) {
 					if(this.data.matoverride == true){
+						console.log("override material = true");
 						node.material.metalness = this.data.metalness;
 						node.material.roughness = this.data.roughness;
 					}
